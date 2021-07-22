4268a63034d09aaf15c0ff71dd1fb4f6b898dda7
Your Name <you@example.com>
2021-07-22 11:11:46 +0300

    One

---
+++ js/main.js
@@ -0,0 +1,254 @@
+/*
+new Swiper('.swiper-container', {
+	loop: true,
+	navigation: {
+		nextEl: '.arrow',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20
+		},
+		541: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active');
+})
+*/
+
+const getElement = (tagName, classNames, attributes) => {
+	const element = document.createElement(tagName);
+
+	if(classNames) {
+		element.classList.add(...classNames);
+	}
+
+	if(attributes) {
+		for(const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
+	}
+
+	return element;
+}
+
+const createHeader = ({ title, header: {logo, social, menu } }) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+	
+
+	if(logo) {
+		const logoImg = getElement('img', ['logo'], {
+			src: logo,
+			alt: 'Логотип ' + title
+		});
+		wrapper.append(logoImg);
+	}
+
+	if(menu) {
+		const menuList = getElement('nav', ['menu-list']);
+		const allMenuLinks = menu.map(item => {
+			const menuLink = getElement('a', ['menu-link'], {
+				href: item.link,
+				textContent: item.title
+			});
+
+			return menuLink;
+		})
+
+		wrapper.append(menuList);
+		menuList.append(...allMenuLinks);
+	}
+
+	if(social) {
+		const socialWrapper = getElement('div',['social']);
+		const allSocial = social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title
+			}));
+			socialLink.href = item.link;
+
+			return socialLink;
+		});
+		wrapper.append(socialWrapper);
+		socialWrapper.append(...allSocial);
+	}
+
+	const menuHumburger = getElement('button', ['menu-button']);
+
+	
+	header.append(container);
+	container.append(wrapper, menuHumburger);
+
+	
+	
+
+	return header;
+};
+
+const createMain = ({ title, main: { genre, rating, description, trailer } }) => {
+
+	const main = getElement('main');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['main-content']);
+	const content = getElement('div', ['content']);
+	
+	if(genre) {
+		const genreSpan = getElement('span', ['genre', 'animated', 'fadeInRight'], {
+			textContent: genre,
+		});
+		content.append(genreSpan);
+	}
+	if(rating) {
+		const ratingBlock = getElement('div', ['rating', 'animated', 'fadeInRight']);
+		const ratingStars = getElement('div', ['rating-stars']);
+		const ratingNumber = getElement('div', ['rating-number'], {
+			textContent: `${rating}/10`
+		});
+
+		for(let i = 0; i < 10; i++) {
+			const star = getElement('img', ['star'], {
+				alt: i ? '' : `Рейтинг ${rating}/10`,
+				src: i < rating ? 'img/star.svg' : 'img/star-o.svg'
+			});
+			ratingStars.append(star);
+		}
+
+		content.append(ratingBlock);
+		ratingBlock.append(ratingStars, ratingNumber);
+	}
+	
+
+	
+
+	main.append(container);
+	container.append(wrapper);
+	wrapper.append(content);
+
+	content.append(getElement('h1', ['main-title', 'animated', 'fadeInRight'], {
+		textContent: title
+	}));
+
+	if(description) {
+		content.append(getElement('p', ['main-description', 'animated', 'fadeInRight'], {
+			textContent: description
+		}));
+	}
+
+	if(trailer) {
+		const youtubeLink = getElement('a', 
+			['button', 'animated', 'fadeInRight', 'youtube-modal'], 
+			{
+				href: trailer,
+				textContent: 'Смотреть трейлер'
+			}
+		);
+
+		const youtubeImageLink = getElement('a', ['play', 'youtube-modal'], {
+			href: trailer,
+			ariaLabel: 'Смотреть трейлер'
+		});
+
+		const iconPlay = getElement('img', ['play-img'], {
+			src: 'img/play.svg',
+			alt: '',
+			ariaHidden: true,
+		});
+
+		content.append(youtubeLink);
+		wrapper.append(youtubeImageLink);
+		youtubeImageLink.append(iconPlay);
+	}
+
+	return main;
+};
+
+const movieConstructor = (selector, options) => {
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+
+	app.style.backgroundImage = options.background ? 
+		`url('${options.background}')` : '';
+
+	document.title = options.title + ' - официальный сайт сериала';
+
+	const favicon = getElement('link', [], {
+		href: options.header.logo,
+		rel: 'shortcut icon',
+		type: 'image/x-icon'
+	})
+
+	document.head.append(favicon)
+
+	if(options.header) {
+		app.append(createHeader(options));
+	}
+
+	if(options.main) {
+		app.append(createMain(options));
+	}
+
+	const menuButton = document.querySelector('.menu-button');
+	const menuElem = document.querySelector('.header');
+	menuButton.addEventListener('click', function () {
+		menuButton.classList.toggle('menu-button-active');
+		menuElem.classList.toggle('header-active');
+	})
+};
+
+movieConstructor('.app', {
+	title: 'Ведьмак',
+	background: 'witcher/background.jpg',
+	header: {
+		logo: 'witcher/logo.png',
+		social: [
+			{
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg'
+			},
+			{
+				title: 'Instagram',
+				link: 'https://instagram.com',
+				image: 'witcher/social/instagram.svg'
+			},
+			{
+				title: 'Facebook',
+				link: 'https://facebook.com',
+				image: 'witcher/social/facebook.svg'
+			}
+		],
+		menu: [
+			{
+				title: 'Описание',
+				link: '#'
+			},
+			{
+				title: 'Трейлер',
+				link: '#'
+			},
+			{
+				title: 'Отзывы',
+				link: '#'
+			},
+		]
+	},
+	main: {
+		genre: '2019,фэнтези',
+		rating: '8',
+		description: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
+		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+	},
+});
+
